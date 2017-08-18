2f2c752d762bdd70f422a7a3af9a1925b9fc6c5b
Yuicon <910722178@qq.com>
2017-08-18 21:45:50 +0800

    perf:使用cdn减小main.js大小

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -84,7 +84,7 @@ module.exports = {
     // https://github.com/facebookincubator/create-react-app/issues/290
     extensions: ['.js', '.json', '.jsx'],
     alias: {
-      
+
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
@@ -114,7 +114,7 @@ module.exports = {
           {
             options: {
               formatter: eslintFormatter,
-              
+
             },
             loader: require.resolve('eslint-loader'),
           },
@@ -160,7 +160,7 @@ module.exports = {
         test: /\.(js|jsx)$/,
         include: paths.appSrc,
         loader: require.resolve('babel-loader'),
-        
+
       },
       // The notation here is somewhat confusing.
       // "postcss" loader applies autoprefixer to our CSS.
@@ -313,5 +313,10 @@ module.exports = {
     fs: 'empty',
     net: 'empty',
     tls: 'empty',
+  },
+  externals: {
+    'react': 'React',
+    'react-dom': 'ReactDOM',
+    'immutable': 'Immutable',
   },
 };
